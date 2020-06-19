2a6c3c2a5c28d425f53ea0bd1713f1139d724122
Amy Barba <Amybarba70@gmail.com>
2020-06-19 01:15:41 -0700

    board is clickablestate has begun

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Conway's Brilliant Game Of Life!</h1>
+        <h1 className="Title">Conway's Brilliant Game Of Life!</h1>
         <Cellular />
       </div>
     );
