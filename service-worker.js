7d0504a58781f9167b33e8f1203ece2c0c96a1dd
Amy Barba <Amybarba70@gmail.com>
2020-06-22 21:06:47 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/practice-conway-game-of-life/precache-manifest.96c6509b127693312f7d86bbe6e055a6.js"
+  "/practice-conway-game-of-life/precache-manifest.118b89c62a8b2b8b26761981ea8cd466.js"
 );
 
 self.addEventListener('message', (event) => {
